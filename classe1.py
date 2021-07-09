cd92d54d3fa766af2fa4ffe498fbccf5a4abe9a0
Lucas Soares <lucassfb@gmail.com>
2021-07-09 17:36:16 -0300

    ajustes finais

--- classe1.py
+++ classe1.py
@@ -1,55 +1,106 @@
-from random import randint
-from time import sleep
+# importação das bibliotecas
 
+from random import randint    # biblioteca para gerar numeros aleatórios 
+from time import sleep       # biblioteca para usar pausas entre linhas de codigo
+
+
+# Criação da classe Nave
 class Nave:
-    def __init__(self, anoAtual, combustivel, planetaPartida, planetaChegada, galaxia, comunicacao):
-        self.anoAtual = anoAtual
-        self.combustivel = combustivel
-        self.planetaPartida = planetaPartida
-        self.planetaChegada = planetaChegada
-        self.galaxia = galaxia
-        self.comunicacao = comunicacao
+    def __init__(self, anoAtual, combustivel, planetaChegada, galaxia, comunicacao, fimJogo, escolha=0):
+        self.__anoAtual = anoAtual
+        self.__combustivel = combustivel
+        self.__planetaChegada = planetaChegada
+        self.__galaxia = galaxia
+        self.__comunicacao = comunicacao
+        self.__fimJogo = fimJogo
+        self.__escolha = escolha
+
+# getter: valor do argumento fimJogo
+    @property
+    def fimJogo(self):
+        return self.__fimJogo
+
+# getter: escolha do planeta pelo usuário
+    @property
+    def escolha(self):
+        return self.__escolha
+
+# método para exibir os valores atribuídos a cada atributo
 
     def __str__(self):
         return f"""
-    Ano Atual: {self.anoAtual}
-    Combustível restante: {self.combustivel}%
-    Planeta de partida: {self.planetaPartida}
-    Planeta de chegada: {self.planetaChegada}
-    Galáxia Atual: {self.galaxia}   
-    Comunicação: {self.comunicacao}   
+                        Ano Atual: {self.__anoAtual}
+             Combustível restante: {self.__combustivel}%
+               Planeta de chegada: {self.__planetaChegada}
+                    Galáxia Atual: {self.__galaxia}   
+                      Comunicação: {self.__comunicacao}   
     """
 
-    def verificaCombustivel(self):
-        if self.combustivel <= 0:
-            return "Você ficou sem combustível."
+# método para finalizar o jogo
+    def gameOver(self):
+        self.__fimJogo = 0
 
+# método para chamar o menu, exibí-lo e requisitar uma informação do usuário para poder viajar entre planetas
     def chamaMenu(self):
         print("Para qual planeta você deseja ir? ")
-        if self.galaxia == "Nova" and self.planetaChegada == "":
+        # se a galáxia for Nova e o planeta de chegada não for nenhum dos possíveis executa:
+        if self.__galaxia == "Nova" and self.__planetaChegada == "":
             print("""
-            1 - Biós (Mais promissor)
-            2 - Agnostos (Planeta similar à Terra)
-            3 - Matér (Pode não ser habitável)        
-            """)
-        elif self.galaxia == "Nova" and self.planetaChegada == "Biós":
+                   1 - Biós (Mais promissor)
+                   2 - Agnostos (Planeta similar à Terra)
+                   3 - Matér (Pode não ser habitável)        
+                """)
+            # enquanto o valor não for nenhum dos possíveis continua solicitando a informação e exibindo opção inválida
+            while True:
+                self.__escolha = int(input("Digite a escolha: "))
+                if self.__escolha == 1 or self.__escolha == 2 or self.__escolha == 3:
+                    break
+                else:
+                    print("Opção inválida.")
+        # se a galáxia for Nova e o planeta de chegada for Biós:
+        elif self.__galaxia == "Nova" and self.__planetaChegada == "Biós":
             print("""
             Vamos tentar outro planeta? 
 
-            2 - Agnostos (Planeta similar à Terra)
-            3 - Matér (Pode não ser habitável)
-            0 - Voltar para Terra       
-            """)
-        elif self.galaxia == "Nova" and self.planetaChegada == "Agnostos":
+                   2 - Agnostos (Planeta similar à Terra)
+                   3 - Matér (Pode não ser habitável)
+                   0 - Voltar para Terra
+                """)
+            # enquanto o valor não for nenhum dos possíveis continua solicitando a informação e exibindo opção inválida
+            while True:
+                self.__escolha = int(input("Digite a escolha: "))
+                if self.__escolha == 0 or self.__escolha == 2 or self.__escolha == 3:
+                    break
+                else:
+                    print("Opção inválida.")
+        # se a galáxia for Nova e o planeta de chegada for Agnostos:
+        elif self.__galaxia == "Nova" and self.__planetaChegada == "Agnostos":
             print("""
-            3 - Matér (Pode não ser habitável)
-            0 - Voltar para Terra           
-            """)
-        elif self.galaxia == "Nova" and self.planetaChegada == "Matér":
+                   3 - Matér (Pode não ser habitável)
+                   0 - Voltar para Terra           
+                """)
+            # enquanto o valor não for nenhum dos possíveis continua solicitando a informação e exibindo opção inválida
+            while True:
+                self.__escolha = int(input("Digite a escolha: "))
+                if self.__escolha == 0 or self.__escolha == 3:
+                    break
+                else:
+                    print("Opção inválida.")
+        # se a galáxia for Nova e o planeta de chegada for Matér:
+        elif self.__galaxia == "Nova" and self.__planetaChegada == "Matér":
             print("""
-            0 - Voltar para Terra         
-            """)
+                   4 - Construir civilização no planeta usando os embriões
+                   0 - Voltar para Terra         
+                """)
+            # enquanto o valor não for nenhum dos possíveis continua solicitando a informação e exibindo opção inválida
+            while True:
+                self.__escolha = int(input("Digite a escolha: "))
+                if self.__escolha == 0 or self.__escolha == 4:
+                    break
+                else:
+                    print("Opção inválida.")
 
+# método para viajar entre os planetas de acordo com o destino escolhido pelo usuário
     def viajarEntrePlanetas(self, destino):
         if destino == 1:
             self.viajaPlanetaBios()
@@ -57,57 +108,60 @@ class Nave:
             self.viajaPlanetaAgnostos()
         elif destino == 3:
             self.viajaPlanetaMater()
-        elif self.planetaChegada == "Biós" and destino == 0:
+
+        elif self.__planetaChegada == "Biós" and destino == 0:
             while destino == 0:
-                print("Coragem!! A humanidade depende de você, por favor não desista.")
+                print('Coragem!! A humanidade depende de você, por favor não desista.')
                 destino = 1
-                # destino = int(input("Digite sua opção: "))
-        elif self.planetaChegada == "Agnostos" and destino == 0:
-            
-            while True:
-                resposta = str(input("Você quer desistir de voltar para a Terra e iniciar uma nova humanidade neste planeta? [S/N]: ")).upper().strip()[0]
-                if resposta == "S":
-                    print("Parabéns pela decisão, mas tente ensinar mais amor e menos ódio para esta nova geração.")
-                    break
-                elif resposta == "N":
+        
+        elif self.__planetaChegada == "Agnostos" and destino == 0:  
                     print("Você voltou para a Terra e encontrou sua filha pela última vez.")
-                    break
-                else:
-                    continue
-        elif self.planetaChegada == "Matér" and destino == 0:
-            print("Seu combustível acabou. É o fim da humanidade...")
-            sleep(4)
-            print("""
-            Espera! Surgiram seres da 5ª dimensão e resolveram nos ajudar para termos outra chance.
-            Você aprendeu VIAGEM NO TEMPO!
+                    self.__galaxia = "Via Láctea"
+                    self.__planetaChegada = "Terra"
+                    self.gameOver()
+        
+        elif self.__planetaChegada == "Matér" and destino == 0:
+            self.__galaxia = "Via Láctea"
+            self.__planetaChegada = "Terra"
+            hist5 = ['Seu combustível acabou. É o fim da humanidade...','Espera! Surgiram seres da 5ª dimensão e resolveram nos ajudar para termos outra chance. Você aprendeu VIAGEM NO TEMPO!', ' Você conseguiu ver sua filha e salvar a humanidade!']
+            for c in hist5:
+                print(c)
+                sleep(4)
+            self.gameOver()
 
-            Você conseguiu ver sua filha e salvar a humanidade!
-            """)
-            
+        elif self.__planetaChegada == "Matér" and destino == 4:
+            hist6 = ['Você iniciou uma nova humanidade!' , 'Faça amor não faça guerra!']
+            for c in hist6:
+                print(c)
+                sleep(3)
+            self.gameOver()
 
-
+# método para criar efeito randômico que aumenta o ano terrestre
     def efeitoRandom(self):
-        resultado = randint(1,5)
+        resultado = randint(1,10)
         if resultado == 1:
             print("Pane elétrica! Você perdeu 2 anos terrestres enquanto reparava a nave.")
-            self.anoAtual += 2
+            self.__anoAtual += 2
         if resultado == 2:
             print("Traje especial despressurizado! Você perdeu 1 ano terrestre aguardando a pressurização.")
-            self.anoAtual += 1
+            self.__anoAtual += 1
 
+# método para alterar entre as galáxias Via Láctea e Nova adicionando um ano terrestre quando muda entre elas
     def mudaGalaxia(self):
-        if self.galaxia == "Via Láctea":
-            self.galaxia = "Nova"
-        elif self.galaxia == "Nova":
-            self.galaxia = "Via Láctea"
-        self.planetaPartida = ""
-        self.planetaChegada = ""
-        self.anoAtual += 1
+        if self.__galaxia == "Via Láctea":
+            self.__galaxia = "Nova"
+            self.__planetaChegada = ""
+        elif self.__galaxia == "Nova":
+            self.__galaxia = "Via Láctea"
+            self.__planetaChegada = "Terra"
+        
+        self.__anoAtual += 1
 
+# método para alterar os parâmetros se o usuario escolhe Biós como destino:
     def viajaPlanetaBios(self):
-        self.planetaChegada = "Biós"
-        self.combustivel -= 10
-        self.anoAtual += 2
+        self.__planetaChegada = "Biós"
+        self.__combustivel -= 10
+        self.__anoAtual += 2
         print('''
         PLANETA BIÓS:
         As informações não estão corretas, o planeta não é habitável, 
@@ -115,23 +169,27 @@ class Nave:
         -10% de combustível
         Se passaram 2 anos terrestres.
         ''')
+        sleep(5)
 
+# método para alterar os parâmetros se o usuario escolhe Agnostos como destino:
     def viajaPlanetaAgnostos(self):
-        self.planetaChegada = "Agnostos"
-        self.combustivel -= 70
-        self.anoAtual += 3
+        self.__planetaChegada = "Agnostos"
+        self.__combustivel -= 70
+        self.__anoAtual += 3
         print('''
         PLANETA AGNOSTOS:
         O planeta é todo coberto por água. Não habitável.
         -70% de combustível
         Se passaram 3 anos terrestres.
         ''')
+        sleep(5)
 
+# método para alterar os parâmetros se o usuario escolhe Matér como destino:
     def viajaPlanetaMater(self):
-        self.planetaChegada = "Matér"
-        self.combustivel -= 20
-        self.anoAtual += 14
-        self.comunicacao = "Instável"
+        self.__planetaChegada = "Matér"
+        self.__combustivel -= 20
+        self.__anoAtual += 14
+        self.__comunicacao = "Instável"
         print('''
         PLANETA MATÉR:
         Planeta muito parecido com a Terra. Habitável.
@@ -139,3 +197,4 @@ class Nave:
         -20% de combustível
         Se passaram 14 anos terrestres.
         ''')
+        sleep(5)
--- main.py
+++ main.py
@@ -1,97 +1,138 @@
-from ast import increment_lineno
-from time import sleep
-from os import system
-from classe1 import Nave
-import pygame
-import os
+# -------------------------------- Libs -------------------------------------
+from time import sleep #Biblioteca para efeitos de transição
+from tqdm import tqdm # Biblioteca para barra de progresso
+from os import system # Biblioteca para utilizar o cls (limpar a tela)
+import pygame # Biblioteca para uso de efeitos sonoros
+import os # Para verificar o caminho da música
+from classe1 import Nave # Importando a classe Nave do arquivo classe1.py
+from functions import cabecalho, galaxiaAtual, galaxiaNova # Importando a função que exibe o cabeçalho
+from functions import fimgame # Importando a função que exibe o Game Over
 
+# ------------------------- Variáveis inicializadas --------------------------
+piloto = "John Doe"
 fimDeJogo = 0
 
-# system("cls")
+# ------------------------------- Programa Principal -------------------------
+if(__name__ == "__main__"): # Só executa o código abaixo se ele estiver no arquivo de nome __main__
 
-# print("Durante um período de escassez de alimentos")
-# sleep(2)
-# print("cientistas descobrem que a Terra tem uma data concreta para acabar...")
-# sleep(2)
-# print("20 anos!!!")
-# sleep(3)
+    system("cls")      # Limpa o a tela do terminal
+    cabecalho()        # Exibe o cabeçalho
 
-# system("cls")
+    for p in tqdm(range(100), ncols=85):          # Barra de carregamento da biblioteca tqdm
+        sleep(0.001)
+    sleep(4)
 
-# print("Um piloto decide deixar sua filha de apenas 10 anos")
-# sleep(2)
-# print("para se arriscar em uma viagem através do universo")
-# sleep(2)
-# print("juntamente com 3 cientistas para identificar um planeta similar à Terra.")
-# sleep(2)
+    system("cls")
+    cabecalho()
 
-system("cls")
+    # Listas com texto introdutório
+    hist1 =['Durante um período de escassez de alimentos\n', 'cientistas descobrem que a Terra tem uma data concreta para acabar em...\n', '20 anos!!!']
+    for c in hist1:       # imprime cada item da lista
+        print(f'{c: ^90}')
+        sleep(5)        
 
-print("Você agora é esse piloto e deve escolher as ações corretas para SALVAR A HUMANIDADE!")
-sleep(2)
-nomePiloto = str(input("Digite seu nome: "))
+    system("cls")
+    cabecalho()
 
-# system("cls")
+    hist2 = ['Um piloto decide deixar sua filha de apenas 10 anos\n', 'para se arriscar em uma viagem através do universo\n','juntamente com 3 cientistas para identificar um planeta similar à Terra.']
+    for c in hist2:
+        print(f'{c: ^90}')    
+        sleep(5)
 
-# print("Sua viagem só é possível através de um BURACO DE MINHOCA!")
-# sleep(2)
-# print("Essa viagem durará 1 ano e nessa nova galáxia você terá 3 planetas promissores.")
-# sleep(2)
-# print("Na nave teremos 4 tripulantes. Você e 3 cientistas.")
-# sleep(2)
+    system("cls")
+    cabecalho()
 
-nave = Nave(2021, 100, "Terra", "", "Via Láctea", "Estável")
-lancarNave = str(input(f"\nVamos lançar nossa nave, {nomePiloto}? [S/N]: ")).upper().strip()[0]
+    hist3 = ['Sua viagem só é possível através de um BURACO DE MINHOCA !\n','O percurso durará 1 ano e nessa nova galáxia você encontrará 3 planetas promissores.\n','Na nave teremos 4 tripulantes. Você e 3 cientistas.\n']
+    for c in hist3:
+        print(f'{c: ^90}')
+        sleep(5)
 
-if lancarNave == "S":
-#     print("Lançamento em: ")
-#     sleep(1)
-#     print("3")
-#     sleep(1)
-#     print("2")
-#     sleep(1)
-#     print("1")
-#     sleep(1)
-#     print("\nVocê está no espaço! Se dirigindo ao buraco de minhoca!")
-#     sleep(2)
-#     print(f"\nO ano atual é 2021 e sua nave está transportando embriões \
-# humanos para caso a volta não seja possível.")
-#     sleep(3)
+    system("cls")
+    cabecalho()
 
-    print(nave)
-    sleep(2)
+    hist4 = ['Você terá que decidir entre seguir o\n', 'Plano A: Encontrar um planeta colonizável e buscar os sobreviventes\n', 'na Terra.\n', 'Ou Plano B: Caso algo dê errado e seu retorno não seja possível\n', 'Sua nave está transportando embriões humanos para a criação de\n','uma nova sociedade.\n']
+    for c in hist4:
+        print(f'{c: ^90}')
+        sleep(5)
 
-    print("Você atravessou o buraco de minhoca!")
-    nave.mudaGalaxia()
-    print(nave)
-    sleep(2)
+    # Nome do piloto-comandante
+    print("Você agora é este piloto e deve escolher as ações corretas para SALVAR A HUMANIDADE !\n")
+    sleep(3)
+    piloto = str(input("                     Digite seu nome, Comandante:   ")).title()
 
-# nave.chamaMenu()
-# escolhaPlaneta = int(input("Digite sua opção: "))
+    system("cls")
+    cabecalho()
 
-while fimDeJogo != 1:
+    nave = Nave(2021, 100, "Terra", "Via Láctea", "Estável", 1) # Instanciando nave na classe Nave
 
-    nave.chamaMenu()
-    escolhaPlaneta = int(input("Digite sua opção: "))
-    nave.efeitoRandom()
-    nave.viajarEntrePlanetas(escolhaPlaneta)
-    print(nave)
-    sleep(2)
+    # Pergunta se o usuário quer lançar a nave
+    lancarNave = str(input(f"                 Vamos lançar nossa nave, {piloto}? [S/N]: ")).upper().strip()[0]
 
-else:
-    print("Lançamento cancelado! Você desistiu e será o FIM DA HUMANIDADE!")
+    # Enquanto a resposta de lancarNave for diferente de S ou N continua perguntando
+    while lancarNave not in 'SN':
+        lancarNave = str(input(f"                 Vamos lançar nossa nave, {piloto}? [S/N]: ")).upper().strip()[0]
+    
+    # Se lancarNave for igual a S, executa:
+    if lancarNave == "S":
+        print(f"{'Lançamento em: ': ^90}")       # Contagem regressiva de 5 até 1
+        for c in range(5 , 0, -1):
+            print(f'{c: ^90}')
+            sleep(1)
 
-# pygame.init()
-# if os.path.exists('David_Bowie_-_Starman-192k.mp3'):
-#   pygame.mixer.music.load('David_Bowie_-_Starman-192k.mp3')
-#   pygame.mixer.music.play()
-#   pygame.mixer.music.set_volume(1)
+        system("cls")
+        galaxiaAtual() # Exibe a imagem do foguete saindo da Terra e indo em direção ao buraco de minhoca
+        sleep(3)
+        
+        print(f"{'Se dirigindo ao buraco de minhoca!': ^90}")
+        sleep(5)
+        system("cls")
+        print(nave) # Exibe as informações sobre a nave
+        sleep(3)
+        print(f"\nO ano atual é 2021 e sua nave está transportando embriões \
+humanos para caso a volta não seja possível.")
+        sleep(4)
+        print("Você atravessou o buraco de minhoca!")
+        galaxiaNova() # Exibe a imagem da nova galáxia
+        sleep(4)
+        system("cls")
+        cabecalho()
+        sleep(3)
 
-#   clock = pygame.time.Clock()
-#   clock.tick(10)
+        nave.mudaGalaxia() # Chama o método que muda de galáxia
+        print(nave)
+        sleep(4)
 
-#   while pygame.mixer.music.get_busy():
-#      pygame.event.poll()
-#      clock.tick(10)
-# else:
-#   print('O arquivo musica.mp3 não está no diretório do script Python')
+
+        # Enquanto fimDeJogo for diferente do valor nave.fimJogo executa o código abaixo
+        while fimDeJogo != nave.fimJogo: 
+            system("cls")
+            cabecalho()
+            nave.chamaMenu() # Método que exibe o menu dependendo do galáxia ou planeta que o usuário se encontra e recebe a informação do usuário do planeta que se deseja ir
+            nave.efeitoRandom() # Efeito randômico que pode gerar conflitos e adicionar anos extras durante os acontecimentos
+            nave.viajarEntrePlanetas(nave.escolha) # Método que altera os valores da nave (exemplo: combustível, anos terrestres, etc.) de acordo com a escolha do usuário
+            print(nave)
+            sleep(8)
+    
+    # Caso o lancarNave seja N exibe:
+
+    else:
+        print("\nVocê agiu conforme seu coração mandou e desistiu de sua missão. Lançamento cancelado! \nVocê viverá o resto de seus dias ao lado de sua família até que chegue o FIM DA HUMANIDADE!")
+        sleep(5)
+    print(fimgame())
+
+    # Executa o som mp3 no final do código
+
+    pygame.init()
+    if os.path.exists('David_Bowie_-_Starman-192k.mp3'):
+      pygame.mixer.music.load('David_Bowie_-_Starman-192k.mp3')
+      pygame.mixer.music.play()
+      pygame.mixer.music.set_volume(1)
+
+      clock = pygame.time.Clock()
+      clock.tick(10)
+
+    while pygame.mixer.music.get_busy():
+        pygame.event.poll()
+        clock.tick(10)
+    else:
+          print('O arquivo musica.mp3 não está no diretório do script Python')
