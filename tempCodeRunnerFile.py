1be5ece0f9d6f2e50fc1bf92758dd08b19f3ea8e
Lucas Soares <lucassfb@gmail.com>
2021-07-09 17:42:43 -0300

    ajustes finais

--- tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-system("cls")
-
-# print("Sua viagem só é possível através de um BURACO DE MINHOCA!")
-# sleep(2)
-# print("Essa viagem durará 1 ano e nessa nova galáxia você terá 3 planetas promissores.")
-# sleep(2)
-# print("Na nave teremos 4 tripulantes. Você e 3 cientistas.")
-# sleep(2)
